dc9e99a1ea319b34a64e2ef28212ae9b927f9a14
Sergey Bakaev <bakaev@yandex.ru>
2018-11-07 23:32:08 +0300

    sqrt implementation

---
+++ ch1.1/sqrt.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(define calc-precision 0.000001)
+
+(define (abs x) ((if (< x 0) - +) 0 x))
+
+(define (sqrt x)
+  (define (good-enough guess) (< (abs (- (* guess guess) x)) calc-precision))
+  (define (improve guess) (/ (+ guess (/ x guess)) 2))
+  (define (sqrt-iter guess) 
+    (if (good-enough guess) 
+        guess 
+        (sqrt-iter (improve guess))))
+
+  (sqrt-iter 1.0)
+)
+
+(abs -1)
+
+(sqrt 2)
+
